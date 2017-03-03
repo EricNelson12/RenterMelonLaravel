3da0e2f8f7a3f73e1f3a920fffb69858bd0c4f9b
Akelly2 <alex_point_zero@hotmail.com>
2017-03-02 21:04:47 -0800

    and forgot the class declaration for it...

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use DB;
 
+class RentalController extends Controller
 {
     function showRentals () {
         $rentals = DB::select('select title, price, description,
