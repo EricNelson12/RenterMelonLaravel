50ece314b89baa58da275f91a30522c3a4bd10ec
Akelly2 <alex_point_zero@hotmail.com>
2017-03-20 21:08:08 -0700

    Admins can view reported ads if they wish.

--- resources/views/admin/reported.blade.php
+++ resources/views/admin/reported.blade.php
@@ -8,7 +8,29 @@
                     <div class="panel-heading">Reported</div>
 
                     <div class="panel-body">
-                        
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <th>User number</th>
+                                <th>Ad number</th>
+                                <th>Reason</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($reported as $report)
+                                    <tr>
+                                        <td><?= $report->id ?></td>
+                                        <td><a href="/rental/<?=$report->rID?>"><?= $report->rID ?></a></td>
+                                        <td><?= $report->reportType ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3">
+                                            <p>
+                                                <?= $report->description ?>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
