065a48a3778e4c28b045ca78d4c472fbedb8a65d
EricNelson12 <ericjnelson012@gmail.com>
2017-03-23 16:08:23 -0700

    Had to edit lines on the .env file and add a new table to to sql DDL to facilitate the password reset feature

--- db/tables_create.sql
+++ db/tables_create.sql
@@ -108,3 +108,11 @@ CREATE TABLE report (
 		ON UPDATE CASCADE
 
 );
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`),
+  KEY `password_resets_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
